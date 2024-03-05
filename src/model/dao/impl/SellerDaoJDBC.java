7a56551b751881fa9505ecdc60addaa895275703
MoisesDN <m.dias009@gmail.com>
2024-03-05 09:48:02 -0300

    findAll implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -63,9 +63,7 @@ public class SellerDaoJDBC implements SellerDao {
 
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
-        }
-
-        finally {
+        } finally {
             DB.closeStatemant(st);
             DB.closeResultSet(rs);
         }
@@ -99,7 +97,43 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public List<Seller> findAll() {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement("SELECT seller.*,department.Name as DepName\n"
+                    + "FROM seller INNER JOIN department\n"
+                    + "ON seller.DepartmentId = department.Id\n"
+                    + "ORDER BY Name");
+
+            rs = st.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                if (dep == null) {
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller obj = instantiateSeller(rs, dep);
+                list.add(obj);
+
+
+            }
+
+            return list;
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatemant(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
@@ -139,9 +173,7 @@ public class SellerDaoJDBC implements SellerDao {
 
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
-        }
-
-        finally {
+        } finally {
             DB.closeStatemant(st);
             DB.closeResultSet(rs);
         }
